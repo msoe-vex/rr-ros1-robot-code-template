6c60717cbddbc83fa88bb55662249e4442ad9a95
Carl <malagodicarl@gmail.com>
2022-02-10 21:24:20 -0600

    Made ClawNode

--- src/v5_hal/firmware/src/nodes/ClawNode.cpp
+++ src/v5_hal/firmware/src/nodes/ClawNode.cpp
@@ -1,6 +1,11 @@
 #include "nodes/ClawNode.h"
 
-ClawNode::ClawNode(NodeManager* node_manager, std::string handle_name): IClawNode(node_manager, handle_name) {
+ClawNode::ClawNode(NodeManager* node_manager, std::string handle_name, ControllerNode* controller, ADIDIgitalOutNode* primary_claw, ADIDIgitalOutNode* back_claw)
+: IClawNode(node_manager, handle_name), 
+m_controller(controller->getController()),
+m_primary_claw(primary_claw),
+m_back_claw(back_claw)
+ {
 
 }
 
@@ -17,7 +22,23 @@ void ClawNode::closeClaw() {
 }
 
 void ClawNode::teleopPeriodic() {
+    bool front_claw_open = false;
+	bool back_claw_open = false;
+	bool l1_current_state = m_controller->get_digital(pros::E_CONTROLLER_DIGITAL_L1);
+	bool l1_previous_state = false;
+	bool a_current_state = m_controller->get_digital(pros::E_CONTROLLER_DIGITAL_A);
+	bool a_previous_state = false;
 
+    if (l1_current_state == 1 && l1_previous_state == 0) {
+			front_claw_open->!front_claw_open;
+		}
+
+	if (a_current_state == 1 && a_previous_state == 0) {
+			back_claw_open = !back_claw_open;
+		}
+
+	l1_previous_state = l1_current_state;
+	a_previous_state = a_current_state;
 }
 
 void ClawNode::autonPeriodic() {
