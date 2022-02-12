6c2a7e778b159b487b6e71754e4420db1481772d
Carl Malagodi <malagodicarl@gmail.com>
2022-02-12 03:48:51 -0600

    comit for fixing

--- src/v5_hal/firmware/include/nodes/ClawNode.h
+++ src/v5_hal/firmware/include/nodes/ClawNode.h
@@ -12,18 +12,21 @@ private:
     bool m_button_previous_state = false;
     ControllerNode* m_controller;
     ADIDigitalOutNode* m__claw;
+    pros::controller_digital_e_t open_button;
+    pros::controller_digital_e_t close_button;
+
     
 public:
     PrimaryClawNode(NodeManager* node_manager, std::string handle_name, ControllerNode* controller,
     ADIDigitalOutNode* claw, pros::controller_digital_e_t open_button, pros::controller_digital_e_t close_button) ;
 
-    virtual void initialize();
+    void initialize();
 
-    virtual void useClaw(bool opened);
+    void useClaw(bool opened);
 
-    virtual void teleopPeriodic();
+    void teleopPeriodic();
 
-    virtual void autonPeriodic();
+    void autonPeriodic();
 
-    virtual ~PrimaryClawNode();
+    ~PrimaryClawNode();
 };
