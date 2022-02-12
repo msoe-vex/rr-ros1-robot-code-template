d5c9431d2e49bda2e7d242b7758174acf94d0b42
Carl <malagodicarl@gmail.com>
2022-02-11 18:21:27 -0600

    Created Basic LiftNode

--- src/v5_hal/firmware/include/nodes/LiftNode.h
+++ src/v5_hal/firmware/include/nodes/LiftNode.h
@@ -3,11 +3,13 @@
 #include "lib-rr/nodes/NodeManager.h"
 #include "lib-rr/nodes/subsystems/ILiftNode.h"
 #include "pros/misc.h"
+#include "lib-rr/util/PID.h
 
 class LiftNode : public ILiftNode {
-
+private:
+    PID m_lift_pid;
 public:
-    LiftNode(NodeManager* node_manager, std::string handle_name, ControllerNode* controller, MotorNode* left_motor, MotorNode* right_motor);
+    LiftNode(NodeManager* node_manager, std::string handle_name, ControllerNode* controller, MotorNode* left_motor, MotorNode* right_motor, ADIDigitalIn* bottom_limit_switch, ADIDigitalIn* top_limit_switch, ADIAnalogIn* potentiometer);
     
      virtual void initialize() = 0;
 
@@ -16,6 +18,8 @@ public:
     virtual void setLiftVelocity(int velocity) = 0;
 
     virtual void setLiftPosition(int position) = 0;
+
+    virtual int getPosition() = 0;
 
     virtual void teleopPeriodic() {};
 
