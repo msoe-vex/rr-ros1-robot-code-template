6fca6fd464a9b0cec0aec96afe70d448fdefb8fe
sb-msoe <brands@msoe.edu>
2022-03-11 18:38:12 -0600

    Auton bugfixes

--- src/v5_hal/firmware/include/nodes/ClawNode.h
+++ src/v5_hal/firmware/include/nodes/ClawNode.h
@@ -12,9 +12,10 @@ private:
     ADIDigitalOutNode* m_claw;
     bool m_claw_open = false;
     bool m_a_previous_state = false;
+    pros::controller_digital_e_t m_button;
 
 public:
-    BackClawNode(NodeManager* node_manager, std::string handle_name, 
+    ClawNode(NodeManager* node_manager, std::string handle_name, 
     ControllerNode* controller, ADIDigitalOutNode* claw, pros::controller_digital_e_t button);
 
     void initialize();
--- src/v5_hal/firmware/src/nodes/ClawNode.cpp
+++ src/v5_hal/firmware/src/nodes/ClawNode.cpp
@@ -3,7 +3,7 @@
 ClawNode::ClawNode(NodeManager* node_manager, std::string handle_name, 
 		ControllerNode* controller, ADIDigitalOutNode* claw, pros::controller_digital_e_t button) : 
 		IClawNode(node_manager, handle_name), 
-		m_controller(controller->getController()),
+		m_controller(controller),
 		m_claw(claw),
 		m_button(button),
 		m_claw_open(false),
@@ -24,7 +24,7 @@ void ClawNode::useClaw(bool opened) {
 }
 
 void ClawNode::teleopPeriodic() {
-	bool a_current_state = m_controller->get_digital(m_button);
+	bool a_current_state = m_controller->getController()->get_digital(m_button);
 
 	if (a_current_state == 1 && m_a_previous_state == 0) {
 			m_claw_open = !m_claw_open;
